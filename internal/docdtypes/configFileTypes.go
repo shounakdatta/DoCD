6522e4bddebfe195883ccb9333c6898b5ad5b0ad
Shounak Datta <shounak14datta@gmail.com>
2020-04-10 21:43:27 -0400

    Add global interal types

---
+++ internal/docdtypes/configFileTypes.go
@@ -0,0 +1,26 @@
+package docdtypes
+
+// Command : Structure of service installation and build commands
+type Command struct {
+	Directory   string
+	Command     string
+	Environment []string
+}
+
+// Service : Structure of configuration services
+type Service struct {
+	ServiceName          string
+	PackageManager       string
+	Path                 string
+	LogFilePath          string
+	InstallationCommands []Command
+	BuildCommands        []Command
+}
+
+// Config : Structure of DOCD-config.json
+type Config struct {
+	ProjectName        string
+	BasePackageManager string
+	InstallServices    bool
+	Services           []Service
+}
